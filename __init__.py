bde0cc1226f5b9ced943895c86fba5dfd07baf4a
mmcelhan <matthewmcelhaney@gmail.com>
2021-05-16 09:42:36 -0600

    added init

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from .market_open import strip_date
+from .market_open import to_string_date
+from .market_open import to_datetime_date
+from .market_open import market_open
--- market_open.py
+++ market_open.py
@@ -1,22 +1,6 @@
 import datetime
 
 
-def strip_date(dt):
-    # takes a datetime and only keeps the date
-    if isinstance(dt, datetime.datetime):
-        dt = datetime.datetime(dt.year, dt.month, dt.day).date()
-
-    return dt
-
-
-def to_datetime_date(date):
-    return datetime.datetime.strptime(str(strip_date(date)), '%Y-%m-%d').date()
-
-
-def to_string_date(date):
-    return str(date)
-
-
 def market_open(date):
     date = strip_date(date)
     return MarketOpen(date).market_open
@@ -32,6 +16,22 @@ def next_market_open_date(date):
 
     next_day = to_string_date(next_day)
     return next_day
+
+
+def strip_date(dt):
+    # takes a datetime and only keeps the date
+    if isinstance(dt, datetime.datetime):
+        dt = datetime.datetime(dt.year, dt.month, dt.day).date()
+
+    return dt
+
+
+def to_datetime_date(date):
+    return datetime.datetime.strptime(str(strip_date(date)), '%Y-%m-%d').date()
+
+
+def to_string_date(date):
+    return str(date)
 
 
 class MarketOpen:
